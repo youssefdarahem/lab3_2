84ece2ba9557659ba432ec40cb76626d14bb6fe3
Youssef Darahem <youssef.darahem@gmail.com>
2024-11-02 14:40:26 +0300

    removed ship class and added manufacturingDate to vehicle

--- Vehicle.java
+++ Vehicle.java
@@ -1,14 +1,7 @@
 public class Vehicle {
     public String name;
     public String type;
-
-    public class ship {
-        int lenght;
-
-        public void display() {
-            System.out.println("this is a ship");
-        }
-    }
+    public String manufacturingDate;
 
     public void run() {
         System.out.println("vehicle is running");
